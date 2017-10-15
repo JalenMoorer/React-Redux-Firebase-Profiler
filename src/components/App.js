9123016359f2a7df2e994cd6e9eed3d873d09d0b
jalen <jalenmmoorer@gmail.com>
2017-10-14 20:14:14 -0400

    Home page set up

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Main from './common/routes/Main';
+
+const App = () => (
+  <div>
+    <Main/>
+  </div>
+);
+
+export default App;
---
+++ src/components/common/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+      <h1>This should be the homepage</h1>
+    </div>
+)
+
+export default Home;
